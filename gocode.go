6cd7fc53e99073cb28b3005b4c764510e05ee1c6
Ying Vang <yingvang30@gmail.com>
2015-02-04 00:34:10 -0800

    First push

---
+++ gocode.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+)
+
+type student struct {
+	name       string
+	testScore1 int
+	testScore2 int
+	average    int
+}
+
+func avg(t1, t2 int) int {
+	var x int = (t1 + t2) / 2
+	return x
+
+}
+
+func modifyAvg(t *student) {
+	*t = student{average: 99, name: t.name}
+}
+
+func main() {
+	var csClass [3]student
+	csClass[0].name = "Mat Jones"
+	csClass[0].testScore1, csClass[0].testScore2 = 77, 93
+	csClass[1].name = "Mike Lee"
+	csClass[1].testScore1, csClass[1].testScore2 = 78, 94
+	csClass[2].name = "John Doe"
+	csClass[2].testScore1, csClass[2].testScore2 = 86, 89
+
+	for i := 0; i < 3; i++ {
+		csClass[i].average = avg(csClass[i].testScore1, csClass[i].testScore2)
+		fmt.Printf("%s, average test score is %d percent.\n", csClass[i].name, csClass[i].average)
+	}
+
+	modifyAvg(&csClass[0])
+	fmt.Printf("now, %s, average test score is %d percent.\n", csClass[0].name, csClass[0].average)
+
+}
